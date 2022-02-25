19225f080887853c065c074780818a1a17e577ee
Yael <yaelgalvanm@gmail.com>
2022-02-24 21:58:55 -0600

    Agregue los nueva contraseña y usuario para la base de datos, ya que la anterior ya caduco

--- src/Modelo/MDB.java
+++ src/Modelo/MDB.java
@@ -17,7 +17,7 @@ public class MDB {
         //conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://127.0.0.1:3306/bddatos", "root","administrador");
 	//public Conexion(String controlador, String contrasena, String URL, String usuario) {
 	//	conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://localhost:3306/bdcostos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&useTimezone=true&serverTimezone=GMT&useSSL=false", "root","administrador");
-        conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5473327", "sql5473327","MLvq4fXmPp");
+        conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5475205", "sql5475205","Kbc7eRyMQm");
         
 
         }
